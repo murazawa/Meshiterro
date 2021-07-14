8f05744483ce7b9ed9dfdcb3b4c249293ec48d8f
murazawa <ahahanoha.m0115@gmail.com>
2021-07-14 14:39:06 +0000

    [Start]4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
   root to: 'homes#top'
   devise_for :users
 
@@ -9,5 +10,6 @@ Rails.application.routes.draw do
     
     resources :post_comments, only: [:create, :destroy]
   end
-
+  resources :users, only: [:show]
+  
  end

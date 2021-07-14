302838285f1fea5e825c9f8233afb2509a765773
murazawa <ahahanoha.m0115@gmail.com>
2021-07-14 15:53:04 +0000

    [Finish]4-5

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
-    
+
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show]
-  
+  resources :users, only: [:show, :edit, :update]
+
  end

ce8caa9896dc6cd25704302339159f947782249a
murazawa <ahahanoha.m0115@gmail.com>
2021-07-14 03:17:56 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '8ff0537f50e878ce55f2f5b21f7c0be252d10061319c5d9c426cecd44ea00419315ad75b0433a5e4fe691aba6b494f33483ce8e0c8baae25b990c6b7496046fa'
